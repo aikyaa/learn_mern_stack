c7162abf751c43477c6cf39c109ec51a3fd8f588
aikya_o <aikya.oruganti@gmail.com>
2025-06-28 22:13:30 +0000

    finished project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from './config/db.js';
+import path from "path";
 
 import productRoutes from "./routes/products.route.js";
 
@@ -9,9 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //allows us to req data
 
 app.use("/api/products", productRoutes);
+
+//deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, '0.0.0.0', () => {
     connectDB();
